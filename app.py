207251d58212b489eee2368a4a20e9705e2b8e58
zljmms32 <zljmms32@126.com>
2017-07-13 13:49:51 +0800

    试验

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def register_routes(app):
 
 # 运行代码
 if __name__ == '__main__':
-    a = configured_app()
+    ap = configured_app()
     # debug 模式可以自动加载你对代码的变动, 所以不用重启程序
     # host 参数指定为 '0.0.0.0' 可以让别的机器访问你的代码
     config = dict(
@@ -47,4 +47,4 @@ if __name__ == '__main__':
         host='0.0.0.0',
         port=3000,
     )
-    a.run(**config)
+    ap.run(**config)
